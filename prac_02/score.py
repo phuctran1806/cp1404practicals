a3e7bc82db6f955041551b79235382b7ef351ba3
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-02-03 23:12:03 +0800

    Add random score generating feature and determine the result

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,17 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+import random
+
 def main():
+    """A program that determine the result from the score."""
     score = float(input("Enter score: "))
-    result = get_result(score)
-    print(result)
+    result = determine_result(score)
+    print(f"Based on your score ({score}), your result is {result}.")
 
+    random_score = random.randint(0, 100)
+    random_result = determine_result(random_score)
+    print(f"From the random score ({random_score}), the random result is {random_result}.")
 
-def get_result(score):
+def determine_result(score):
+    """Get the result based on score."""
     if score < 0 or score > 100:
         return "Invalid score"
     if score >= 90:
