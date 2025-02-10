1cd13515032f454df51c0ccf2b24875f9e63ac39
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-02-10 20:15:41 +0800

    Changed some variables' names

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,9 +1,11 @@
+# 1
 name = input("Enter your name: ")
-out_file = open(f"{name}.txt", "w")
-print(name, file=out_file)
-out_file.close()
+out_file_name = open(f"{name}.txt", "w")
+print(name, file=out_file_name)
+out_file_name.close()
 
-in_file = open(f"{name}.txt", "r")
-in_file_name = in_file.readline()
-print(f"Hi {in_file_name.strip()}!")
-in_file.close()
+# 2
+in_file_name = open(f"{name}.txt", "r")
+in_file_name_content = in_file_name.readline()
+print(f"Hi {in_file_name_content.strip()}!")
+in_file_name.close()
