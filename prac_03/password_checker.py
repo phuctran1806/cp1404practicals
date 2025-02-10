f7630d249bf4354119a9b1b31574f070297d3e02
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-02-10 19:50:28 +0800

    Added special characters checker

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -50,6 +50,12 @@ def is_valid_password(password):
 
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
+    if IS_SPECIAL_CHARACTER_REQUIRED:
+        for special_character in SPECIAL_CHARACTERS:
+            if special_character in password:
+                number_of_special += 1
+        if number_of_special == 0:
+            return False
 
     # if we get here (without returning False), then the password must be valid
     return True
