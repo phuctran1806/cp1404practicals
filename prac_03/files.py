f30451a3c719eb10e6344569e2358b0b6e0c496f
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-02-10 20:09:52 +0800

    Added file loading and reading feature

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,7 @@ out_file = open(f"{name}.txt", "w")
 print(name, file=out_file)
 out_file.close()
 
+in_file = open(f"{name}.txt", "r")
+in_file_name = in_file.readline()
+print(f"Hi {in_file_name.strip()}!")
+in_file.close()
