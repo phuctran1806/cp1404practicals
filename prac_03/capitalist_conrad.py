7c8942f5a895db8b814b837b86e808e3b56ce69a
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-02-10 18:16:30 +0800

    Added file saving feature

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,10 +14,13 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1
 MAX_PRICE = 100
 INITIAL_PRICE = 10.0
+FILENAME = "capitalist_conrad.txt"
 
 number_of_days = 1
 price = INITIAL_PRICE
-print(f"Starting price: ${price:,.2f}")
+
+out_file = open(FILENAME, "w")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -33,5 +36,7 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
     number_of_days += 1
+
+out_file.close()
