d0adf9d7068506a90ea2f8dd00bd7b3732b7e495
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-02-10 20:30:49 +0800

    Added code that read first 2 numbers from a file and add them up

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,3 +9,11 @@ in_file_name = open(f"{name}.txt", "r")
 in_file_name_content = in_file_name.readline()
 print(f"Hi {in_file_name_content.strip()}!")
 in_file_name.close()
+
+# 3
+total = 0
+with open("numbers.txt", "r") as in_file_numbers:
+    lines = in_file_numbers.readlines()[:2]
+    for line in lines:
+        total += int(line.strip())
+print(total)
