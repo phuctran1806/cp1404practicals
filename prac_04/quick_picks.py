68176e153d69ddccfdfd51b2da4c214e89606c24
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-02-17 20:13:05 +0800

    Made the quick pick has no repeated numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,9 +5,16 @@ MAXIMUM = 45
 NUMBERS_PER_ROW = 6
 
 number_of_quick_picks = int(input("How many quick picks? "))
+while number_of_quick_picks < 0:
+    print("Invalid input. Try again.")
+    number_of_quick_picks = int(input("How many quick picks? "))
 
 for i in range(number_of_quick_picks):
+    quick_pick = []
     for j in range(NUMBERS_PER_ROW):
         random_number = random.randint(MINIMUM, MAXIMUM)
-        print(f"{random_number:2}", end=" ")
-    print()
+        while random_number in quick_pick:
+            random_number = random.randint(MINIMUM, MAXIMUM)
+        quick_pick.append(random_number)
+    quick_pick.sort()
+    print(" ".join([f"{number:2}" for number in quick_pick]))
