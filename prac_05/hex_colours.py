79c46350c6694383cde5a8000ed97f63d5a78495
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-01 13:19:42 +0800

    Added COLOUR_TO_HEX dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,13 @@
+COLOUR_TO_HEX = {
+    "AliceBlue": "#f0f8ff",
+    "Amaranth": "#e52b50",
+    "Amber": "#ffbf00",
+    "Amethyst": "#9966cc",
+    "AntiqueWhite": "#faebd7",
+    "Aqua": "#00ffff",
+    "Beaver": "#9f8170",
+    "Beige": "#f5f5dc",
+    "Black": "#000000",
+    "Bole": "#79443b",
+}
+print(COLOUR_TO_HEX)
