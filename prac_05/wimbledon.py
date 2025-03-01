ec96546d66082392cad063490b126639f4f0b6f3
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-01 15:11:18 +0800

    Completed prac 5 Wimbledon exercise

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,11 +7,13 @@ FILENAME = "wimbledon.csv"
 COUNTRY_INDEX = 1
 CHAMPION_INDEX = 2
 
+
 def main():
     """Load, process, and display the data."""
     data = load_data(FILENAME)
     champion_to_win_count, champion_countries = process_data(data)
     display_result(champion_to_win_count, champion_countries)
+
 
 def display_result(champion_to_win_count, champion_countries):
     """Display all the champions with their win count, and the champion countries."""
@@ -22,6 +24,7 @@ def display_result(champion_to_win_count, champion_countries):
     print(f"\nThere are {len(champion_countries)} countries have won Wimbledon:")
     print(", ".join(champion_countries))
 
+
 def process_data(data):
     """Store the champions associated with their win count, and the champion countries."""
     champion_to_win_count = {}
@@ -30,6 +33,7 @@ def process_data(data):
         champion_to_win_count[row[CHAMPION_INDEX]] = champion_to_win_count.get(row[CHAMPION_INDEX], 0) + 1
         countries.add(row[COUNTRY_INDEX])
     return champion_to_win_count, countries
+
 
 def load_data(filename):
     """Load data and store each row in a list."""
@@ -40,5 +44,6 @@ def load_data(filename):
             parts = line.strip().split(",")
             data.append(parts)
     return data
+
 
 main()
