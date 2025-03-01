e54814c541fe1cf4383460b82610d6d31407fd58
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-01 14:40:53 +0800

    Completed the task of getting the champions and their win count

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,5 +1,27 @@
 """
 Wimbledon
-Estimate: 15 minutes
+Estimate: 30 minutes
 Actual: 15 minutes
 """
+import csv
+
+FILENAME = "wimbledon.csv"
+
+def main():
+    champion_to_count = retrieve_champion_to_count(FILENAME)
+
+    print("Wimbledon Champions:")
+    for champion, count in champion_to_count.items():
+        print(champion, count)
+
+def retrieve_champion_to_count(filename):
+    champion_to_count = {}
+    with open(filename, "r", encoding="utf-8-sig") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        for row in csv_reader:
+            champion_to_count[row[2]] = champion_to_count.get(row[2], 0) + 1
+    return champion_to_count
+
+
+main()
