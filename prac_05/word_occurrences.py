3c0473573d5699f4996c6aba1526943afbee2d2f
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-01 13:38:09 +0800

    Count the word and sort the word_to_count dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,14 @@ Word Occurrences
 Estimate: 20 minutes
 Actual:
 """
+from operator import itemgetter
+
+word_to_count = {}
+
+input_string = input("Text: ")
+for word in input_string.split():
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+
+for word, count in sorted(word_to_count.items(), key=itemgetter(0)):
+    print(f"{word}: {count}")
