183572495f38d3a52a88c71f2fa7e1f9b7c7db7c
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-01 13:29:03 +0800

    Completed prac 5 hex_colours.py exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -11,3 +11,13 @@ COLOUR_TO_HEX = {
     "Bole": "#79443b",
 }
 print(COLOUR_TO_HEX)
+
+COLOUR_TO_HEX_LOWER = {colour.lower(): hex for colour, hex in COLOUR_TO_HEX.items()} # Convert all keys of COLOUR_TO_HEX to lowercase
+
+colour_name = input("Enter colour name: ").lower().strip()
+while colour_name != "":
+    try:
+        print(f"{colour_name} is {COLOUR_TO_HEX[colour_name]}")
+    except KeyError:
+        print(f"{colour_name} is not a valid colour.")
+    colour_name = input("Enter colour name: ").lower().strip()
