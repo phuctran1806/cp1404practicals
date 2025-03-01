6c4603946476811365e03a1ce1d0f19c17b61741
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-01 15:04:12 +0800

    Store data into a dictionary and a set

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,10 +11,19 @@ CHAMPION_INDEX = 2
 
 def main():
     data = load_data(FILENAME)
+    champion_to_win_count, countries = process_data(data)
 
-
+def process_data(data):
+    """Store the champions associated with their win count, and the champion countries."""
+    champion_to_win_count = {}
+    countries = set()
+    for row in data:
+        champion_to_win_count[row[CHAMPION_INDEX]] = champion_to_win_count.get(row[CHAMPION_INDEX], 0) + 1
+        countries.add(row[COUNTRY_INDEX])
+    return champion_to_win_count, countries
 
 def load_data(filename):
+    """Load data and store each row in a list."""
     data = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
