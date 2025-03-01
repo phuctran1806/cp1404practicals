ba55adc5e41eb2d6f58ba117f4d8f2346a707fe5
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-01 13:40:54 +0800

    Completed prac 5 Word Occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -11,6 +11,8 @@ input_string = input("Text: ")
 for word in input_string.split():
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
+width = max((len(word) for word in word_to_count.keys()))
+print(width)
 
 for word, count in sorted(word_to_count.items(), key=itemgetter(0)):
-    print(f"{word}: {count}")
+    print(f"{word:{width}} : {count}")
