af57d07b6020717d5fbe76f805683bebe862ebb2
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-03 17:41:07 +0800

    Fixed the get_age() method

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,6 +3,10 @@ Guitars! exercise
 Estimated time:
 Actual time:
 """
+from datetime import datetime
+
+VINTAGE_THRESHOLD = 50
+
 class Guitar:
     """Represents a Guitar object."""
     def __init__(self, name="", year=0, cost=0):
@@ -15,10 +19,10 @@ class Guitar:
         """Return a string representation of the Guitar object."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
-    def get_age(self, current_year):
+    def get_age(self):
         """Return the age of the Guitar."""
-        return current_year - self.year
+        return datetime.now().year - self.year
 
-    def is_vintage(self, current_year):
+    def is_vintage(self):
         """Determine if the Guitar is vintage."""
-        return self.get_age(current_year) > 50
+        return self.get_age() > VINTAGE_THRESHOLD
