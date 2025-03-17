de0c00a355e2e9e4e8d42496ebaa19a96c4bc304
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-03-17 15:00:46 +0800

    Added the feature that ask the user the add their new guitars

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,8 +8,29 @@ COST_INDEX = 2
 def main():
     guitars = load_guitars(FILENAME)
     guitars.sort()
+    display_guitars(guitars)
+
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        guitar_year = int(input("Year: "))
+        guitar_cost = float(input("Cost: $"))
+        add_guitar(guitars, guitar_name, guitar_year, guitar_cost)
+        guitar_name = input("\nName: ")
+
+
+
+def add_guitar(guitars, guitar_name, guitar_year, guitar_cost):
+    guitar = Guitar(guitar_name, guitar_year, guitar_cost)
+    guitars.append(guitar)
+    print(f"{guitar.name} ({guitar.year}) : ${guitar.cost:,.2f} added.")
+
+
+def display_guitars(guitars):
     for guitar in guitars:
         print(guitar)
+
+
+
 
 
 def load_guitars(filename):
