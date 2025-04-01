2d2b4340ab5623d0e92448a718df00b523d00695
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-04-01 10:53:01 +0800

    Drive the car if the random generated number is less than the car's reliability

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,3 +1,4 @@
+import random
 from prac_09.car import Car
 
 class UnreliableCar(Car):
@@ -6,3 +7,7 @@ class UnreliableCar(Car):
         super().__init__(name, fuel)
         self.reliability = max(0, min(100, reliability))  # Clamps percentage value between 0 and 100
 
+    def drive(self, distance):
+        """Drive the car with a given distance if the random number is less than the car's reliability."""
+        random_number = random.randint(0, 100)
+        return super().drive(distance) if random_number < self.reliability else 0
