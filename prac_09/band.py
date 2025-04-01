199eeb235b725e353813fd1c84ec5bb95d7b1b6b
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-04-01 12:08:10 +0800

    Return a string representative of the Band object

--- prac_09/band.py
+++ prac_09/band.py
@@ -4,6 +4,11 @@ class Band:
         self.name = name
         self.musicians = []
 
+    def __str__(self):
+        """Return a string representation of the Band object."""
+        musician_as_str = ", ".join(str(musician) for musician in self.musicians)
+        return f"{self.name} ({musician_as_str})"
+
     def add(self, musician):
         """Add a musician to the collection of musicians."""
         self.musicians.append(musician)
