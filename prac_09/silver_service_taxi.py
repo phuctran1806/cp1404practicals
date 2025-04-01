5120913798bfb812014f5e1c6df965c6d8ec3869
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-04-01 11:10:24 +0800

    Fixed spelling mistake

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,7 +2,7 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     def __init__(self, name, fuel, fanciness):
-        """Initialize a SivlerServiceTaxi object."""
+        """Initialize a SilverServiceTaxi object."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
