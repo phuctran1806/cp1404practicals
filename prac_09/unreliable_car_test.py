29cf18fa84dcd297f5849f46c56d611d04f3126b
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-04-01 11:00:57 +0800

    Test two cars, one is reliable, the other is unreliable

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,5 +1,13 @@
 from prac_09.unreliable_car import UnreliableCar
 
-my_unreliable_car = UnreliableCar("Toyota Innova", 100, 30)
-for i in range(10):
-    print(f"Test {i+1}. Distance driven: {my_unreliable_car.drive(10)}")
+reliable_car = UnreliableCar("Reliable Car", 100, 95)
+unreliable_car = UnreliableCar("Unreliable Car", 100, 30)
+
+for i in range(1, 11):
+    print(f"Test {i+1}:")
+    print(f"{reliable_car.name} drove {reliable_car.drive(i)}km")
+    print(f"{unreliable_car.name} drove {unreliable_car.drive(i)}km")
+
+print("\nFinal state of each car after testing:")
+print(reliable_car)
+print(unreliable_car)
