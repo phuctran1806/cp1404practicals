6d4dc9d5b998620644a116231e30d8536c32a18e
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-04-01 11:38:55 +0800

    Test the get_fare() method to see if the fare is rounded

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -5,4 +5,4 @@ print(silver_service_taxi)  # Test the __str__() method.
 
 # Test the get_fare() method after an 18-km trip.
 silver_service_taxi.drive(18)
-assert silver_service_taxi.get_fare() == 48.78
+assert silver_service_taxi.get_fare() == 48.80
