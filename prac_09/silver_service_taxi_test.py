0ab0f8eda47859892caf4dc45fe5f33915b2e345
ADMIN <phangiaphuc.tran@my.jcu.edu.au>
2025-04-01 11:28:32 +0800

    Tested the __str__() method and the get_fare() method

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,8 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+silver_service_taxi = SilverServiceTaxi("Hummer", 200, 2)
+print(silver_service_taxi)  # Test the __str__() method.
+
+# Test the get_fare() method after an 18-km trip.
+silver_service_taxi.drive(18)
+assert silver_service_taxi.get_fare() == 48.78
